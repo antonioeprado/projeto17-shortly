fd960675b54baf16ce563a4dd39b2026437363d0
Antonio Prado <antonioeprado18@gmail.com>
2022-12-15 15:27:12 -0300

    feat: sign up model

---
+++ src/models/userSignUp.model.js
@@ -0,0 +1,8 @@
+import Joi from "joi";
+
+export const userSignUpModel = Joi.object({
+	name: Joi.string().min(3).required(),
+	email: Joi.string().email().required(),
+	password: Joi.string().pattern(new RegExp("^[a-zA-Z0-9]{4,24}$")).required(),
+	repassword: Joi.ref("password"),
+});
