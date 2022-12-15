4e6b006b5459774118de63bf96408c215b6abe5d
Antonio Prado <antonioeprado18@gmail.com>
2022-12-15 15:28:56 -0300

    feat: sign up payload validation and password encryption middlewares

---
+++ src/middlewares/users.middlewares.js
@@ -0,0 +1,19 @@
+import { Hash } from "../hashPassword.js";
+import ValidateModel from "../validateModel.js";
+
+export const validatePayload = async (req, res, next) => {
+	const user = new ValidateModel(req.body, "users");
+	if (!user.status) return res.status(422).send(user.errors);
+	if (await user.isUsed) return res.sendStatus(409);
+	delete req.body.repassword;
+	next();
+};
+
+export const encryptPassword = (req, res, next) => {
+	const { password, ...rest } = req.body;
+	res.locals.user = {
+		...rest,
+		password: Hash.encrypt(password),
+	};
+	next();
+};
