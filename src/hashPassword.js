cc51eed42d8cd4d58719c8c58664b5d67c5006b0
Antonio Prado <antonioeprado18@gmail.com>
2022-12-21 18:55:48 -0300

    style: changed function method name

--- src/hashPassword.js
+++ src/hashPassword.js
@@ -2,5 +2,5 @@ import bcrypt from "bcrypt";
 
 export const Hash = {
 	encrypt: (password) => bcrypt.hashSync(password, 10),
-	compare: (password, hash) => bcrypt.compareSync(password, hash),
+	decrypt: (password, hash) => bcrypt.compareSync(password, hash),
 };
