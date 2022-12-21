d12b42c2a2e7369f235fa4fdba7ca99b2e8b8228
Antonio Prado <antonioeprado18@gmail.com>
2022-12-21 18:59:23 -0300

    feat: user sign in

--- src/controllers/users.controllers.js
+++ src/controllers/users.controllers.js
@@ -1,10 +1,13 @@
 import { connection } from "../database/db.js";
+import jwt from "jsonwebtoken";
+// import dotenv from 'dotenv';
+// dotenv.config();
 
 export const signUp = async (req, res) => {
 	try {
 		const { name, email, password } = res.locals.user;
 		await connection.query(
-			`INSERT INTO users (Username, Email, Password) VALUES ($1, $2, $3)`,
+			`INSERT INTO users (username, email, password) VALUES ($1, $2, $3)`,
 			[name, email, password]
 		);
 		res.sendStatus(201);
@@ -13,6 +16,9 @@ export const signUp = async (req, res) => {
 	}
 };
 
-export const signIn = async (req, res) => {};
+export const signIn = (req, res) => {
+	const token = jwt.sign(res.locals.id, process.env.JWT_SECRET);
+	res.status(200).send({ token });
+};
 
 export const userProfile = async (req, res) => {};
