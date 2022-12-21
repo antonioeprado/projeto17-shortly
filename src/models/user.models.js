618cc5aa97768a106efaa880d0ddb3312f937980
Antonio Prado <antonioeprado18@gmail.com>
2022-12-21 18:58:58 -0300

    refactor: joined models in the same file

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -6,3 +6,8 @@ export const userSignUpModel = Joi.object({
 	password: Joi.string().pattern(new RegExp("^[a-zA-Z0-9]{4,24}$")).required(),
 	repassword: Joi.ref("password"),
 });
+
+export const userSignInModel = Joi.object({
+	email: Joi.string().email().required(),
+	password: Joi.string().pattern(new RegExp("^[a-zA-Z0-9]{4,24}$")).required(),
+});
