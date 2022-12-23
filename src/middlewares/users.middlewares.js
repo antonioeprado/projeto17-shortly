1a240249dbc4ec52d6dc7a9d9517f335235855a6
Antonio Prado <antonioeprado18@gmail.com>
2022-12-22 21:34:09 -0300

    fix: model string name

--- src/middlewares/users.middlewares.js
+++ src/middlewares/users.middlewares.js
@@ -2,7 +2,7 @@ import { Hash } from "../hashPassword.js";
 import ValidateModel from "../validateModel.js";
 
 export const validatePayload = async (req, res, next) => {
-	const user = new ValidateModel(req.body, "users");
+	const user = new ValidateModel(req.body, "signup");
 	if (!user.status) return res.status(422).send(user.errors);
 	if (await user.isUsed) return res.sendStatus(409);
 	delete req.body.repassword;
