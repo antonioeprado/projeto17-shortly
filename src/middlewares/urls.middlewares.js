5d7023085f0921306743c04d0cdfdc6fb1f95325
Antonio Prado <antonioeprado18@gmail.com>
2022-12-23 09:51:22 -0300

    feat: validate url ownership middlewares

--- src/middlewares/urls.middlewares.js
+++ src/middlewares/urls.middlewares.js
@@ -11,13 +11,31 @@ export const validateUrl = (req, res, next) => {
 export const findUrl = async (req, res, next) => {
 	try {
 		const urlQuery = await connection.query(
-			`SELECT url_id, url FROM urls WHERE short_url=$1`,
+			`SELECT url_id, url, user_id FROM urls WHERE short_url=$1`,
 			[req.params.shortUrl]
 		);
 		if (!urlQuery.rowCount) return res.sendStatus(404);
-		res.locals.url = { url: urlQuery.rows[0].url, id: urlQuery.rows[0].url_id };
+		res.locals.url = {
+			url: urlQuery.rows[0].url,
+			id: urlQuery.rows[0].url_id,
+		};
 		next();
 	} catch (error) {
 		console.log(error);
 	}
+};
+
+export const findUrlById = async (req, res, next) => {
+	const urlQuery = await connection.query(
+		`SELECT * FROM urls WHERE url_id=$1`,
+		[req.params.id]
+	);
+	if (!urlQuery.rowCount) return res.sendStatus(404);
+	res.locals.urlOwner = urlQuery.rows[0].user_id;
+	next();
+};
+
+export const validateOwnership = (req, res, next) => {
+	if (res.locals.urlOwner != res.locals.user_id) return res.sendStatus(401);
+	next();
 };
