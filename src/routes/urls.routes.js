35371dd9afe4f5c156eeb6b4aaf756688cb8354c
Antonio Prado <antonioeprado18@gmail.com>
2022-12-22 22:13:26 -0300

    feat: redirect route by url reference

--- src/routes/urls.routes.js
+++ src/routes/urls.routes.js
@@ -1,12 +1,18 @@
 import { Router } from "express";
-import { shortenUrl, urlById } from "../controllers/urls.controllers.js";
+import {
+	redirect,
+	shortenUrl,
+	urlById,
+} from "../controllers/urls.controllers.js";
 import { verifyToken } from "../middlewares/auth.middlewares.js";
-import { validateUrl } from "../middlewares/urls.middlewares.js";
+import { findUrl, validateUrl } from "../middlewares/urls.middlewares.js";
 
 const router = Router();
 
 router.post("/urls/shorten", verifyToken, validateUrl, shortenUrl);
 
 router.get("/urls/:id", urlById);
+
+router.get("/urls/open/:shortUrl", findUrl, redirect);
 
 export default router;
