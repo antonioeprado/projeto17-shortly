b2afb3f5e25e28a293daf6f24437f2932965f671
Antonio Prado <antonioeprado18@gmail.com>
2022-12-23 09:51:50 -0300

    feat: url delete route with middlewares

--- src/routes/urls.routes.js
+++ src/routes/urls.routes.js
@@ -1,11 +1,17 @@
 import { Router } from "express";
 import {
+	deleteUrl,
 	redirect,
 	shortenUrl,
 	urlById,
 } from "../controllers/urls.controllers.js";
 import { verifyToken } from "../middlewares/auth.middlewares.js";
-import { findUrl, validateUrl } from "../middlewares/urls.middlewares.js";
+import {
+	findUrl,
+	findUrlById,
+	validateOwnership,
+	validateUrl,
+} from "../middlewares/urls.middlewares.js";
 
 const router = Router();
 
@@ -14,5 +20,13 @@ router.post("/urls/shorten", verifyToken, validateUrl, shortenUrl);
 router.get("/urls/:id", urlById);
 
 router.get("/urls/open/:shortUrl", findUrl, redirect);
+
+router.delete(
+	"/urls/:id",
+	verifyToken,
+	findUrlById,
+	validateOwnership,
+	deleteUrl
+);
 
 export default router;
